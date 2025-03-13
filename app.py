1f72c0787dc6019c888215acc270d410970f0807
oslodanne <andersson.daniel@live.com>
2025-03-13 17:54:43 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -34,6 +34,5 @@ def index():
     return render_template('index.html', artikelnummer=artikelnummer)
 
 if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    app.run(debug=True)
 
