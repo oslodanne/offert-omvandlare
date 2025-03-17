470c18b07c242ce9743653899299655e2eae1452
oslodanne <andersson.daniel@live.com>
2025-03-17 01:13:31 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template
 import re
 
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 # Artikelregister med olika varianter av kundens benämning
 artikelregister = {
